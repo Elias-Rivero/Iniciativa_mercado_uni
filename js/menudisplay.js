8e54092b7d8f8cc046a760f9ce3f3c0cdee210e3
Empire <eriverotamayo7@gmail.com>
2024-12-18 19:41:14 -0500

    Subiendo el conjunto de archivos que conforman el proyecto.

---
+++ js/menudisplay.js
@@ -0,0 +1,11 @@
+function Open(){
+    document.getElementById("Menu").style.display = "block";
+}
+
+function CloseNav(){
+    document.getElementById("Menu").style.display = "none"
+}
+
+function Hrefhome(){
+    window.location.href = "Trabajo_web.html";
+}
